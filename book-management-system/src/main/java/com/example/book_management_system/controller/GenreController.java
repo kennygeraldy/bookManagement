e76ccd5a87d270e2357a32809c21af945f45c348
Kenny Geraldy Chandra <kennygeraldy@gmail.com>
2024-09-09 17:27:39 +0700

    add controller

--- book-management-system/src/main/java/com/example/book_management_system/controller/GenreController.java
+++ book-management-system/src/main/java/com/example/book_management_system/controller/GenreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/genres")
 public class GenreController {
 
     @Autowired
