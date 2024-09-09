64a3768d3b78a86502d5c2ab70af97cf77119394
Tasya <tasyaraishania@gmail.com>
2024-09-09 16:38:34 +0700

    add book repository

---
+++ book-management-system/src/main/java/com/example/book_management_system/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package com.example.book_management_system.repository;
+
+import com.example.book_management_system.entity.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    Optional<BookEntity> findByTitleIgnoreCase (String title);
+
+    Optional<BookEntity> findByAuthorIgnoreCase (String author);
+
+    Optional<BookEntity> findByGenreIgnoreCase (String genre);
+
+    Page<BookEntity> findAll(Pageable pageable);
+}
