c8f876e13c83e8005d6f831e74d7bdad879f2092
Kenny Geraldy Chandra <kennygeraldy@gmail.com>
2024-09-09 17:22:56 +0700

    add genre Controller

---
+++ book-management-system/src/main/java/com/example/book_management_system/controller/GenreController.java
@@ -0,0 +1,75 @@
+package com.example.book_management_system.controller;
+
+
+import com.example.book_management_system.entity.GenreEntity;
+import com.example.book_management_system.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class GenreController {
+
+    @Autowired
+    private GenreService genreService;
+
+    @GetMapping()
+    public List<GenreEntity> getAllGenres() {
+        return genreService.getAllGenres();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreEntity> getGenreById(
+            @PathVariable Long id
+    ){
+        GenreEntity genre = genreService.getGenreById(id);
+        if (genre != null) {
+            return ResponseEntity.ok(genre);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //  Create Genre
+    @PostMapping
+    public GenreEntity createGenre(@RequestBody GenreEntity genre) {
+        return genreService.saveGenre(genre);
+    }
+
+    //   Put Genres
+    @PutMapping("/{id}")
+    public ResponseEntity<GenreEntity> updateGenre(@PathVariable Long id, @RequestBody GenreEntity genreDetails) {
+        GenreEntity genre = genreEntity.getGenreById(id);
+        if (genre != null) {
+            genre.setBook(genreDetails.getBook());
+            genre.setGenre(genreDetails.getGenre());
+            GenreEntity updatedGenre = genreService.saveGenre(genre);
+            return ResponseEntity.ok(updatedGenre);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Search book by genre
+    @GetMapping("/search")
+    public ResponseEntity<GenreEntity> getBookByGenre(@RequestParam(name = "title") String genre) {
+        GenreEntity genre = genreService.getGenreByGenre(genre);
+        if (genre != null) {
+            return ResponseEntity.ok(genre);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //   Get all genres with pagination
+    @GetMapping("/page")
+    public Page<GenreEntity> getAllGenres(Pageable pageable) {
+        return genreService.getGenres(pageable);
+    }
+
+}
